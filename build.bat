c6ea5041591ca145138f956ed5bada6553751951
Bao Haojun <baohaojun@gmail.com>
2017-04-10 17:46:59 +0800

    Release for V3.1.4 : SnoreNotify (d2f2431)

---
+++ build.bat
@@ -0,0 +1,2 @@
+set path=c:/Qt/Qt5.8.0/5.8/mingw53_32/bin;c:/Qt/Qt5.8.0/Tools/mingw530_32/bin;c:/cmake-w32/cmake-3.8.0-rc1-win32-x86/bin;%path%
+windeployqt --qmldir z:/home/bhj/src/github/snorenotify/src/plugins/backends/snore -qml -quick .
